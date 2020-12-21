7d3d5a93c4f9239fe443c71376250333dd8d51fc
Berongar <Andropov.M.V@gmail.com>
2020-12-21 22:59:45 +0300

    изменил внешний вид приложение. добавил Tab Widget

--- pyqt_lesson3.py
+++ pyqt_lesson3.py
@@ -3,6 +3,9 @@ from mydesign import Ui_MainWindow  # импорт нашего сгенерир
 import sys
 import math
 
+# pyuic5 mydesign.ui -o mydesign.py
+# pyinstaller -w --onefile pyqt_lesson3.py
+
 
 class mywindow(QtWidgets.QMainWindow):
 
@@ -12,6 +15,7 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.ui.Button_Sechenie.clicked.connect(self.btnClicked_sechenie)
         self.ui.Button_Diametr.clicked.connect(self.btnClicked_diametr)
+        self.ui.Button_Rashod.clicked.connect(self.btnClicked_rashod)
 
     def btnClicked_sechenie(self):
         Diametr = float(self.ui.Diametr_input.text())
@@ -25,6 +29,11 @@ class mywindow(QtWidgets.QMainWindow):
         Diametr = str(round(math.sqrt(Sechenie/0.785), 2))
         self.ui.Diametr_out.setText(Diametr)
         self.ui.Diametr_out.adjustSize()
+    def btnClicked_rashod(self):
+        M3InHour = float(self.ui.Rashod_input_m3.text())
+        LiterInHour = str(round(((M3InHour*1000)/60), 2))
+        self.ui.Rashod_out_LinH.setText(LiterInHour)
+        self.ui.Rashod_out_LinH.adjustSize()
 
 
 app = QtWidgets.QApplication([])
